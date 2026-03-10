bf202902611a6ffaab4548707c8f64a410ec24f8
Chethana45 <chethanasriadusumalli@gmail.com>
2026-03-10 16:50:19 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -274,5 +274,8 @@ def run_code():
 
 # ---------------- START SERVER ----------------
 
+# ---------------- START SERVER ----------------
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
